5a072eed13a6ffe4f707ee011a1446d0f4fd57ed
wonita2 <106142369+wonita2@users.noreply.github.com>
2023-03-09 18:04:58 +0200

    object orientated
    working on a project

--- lecture 1/lecture 1 another assembly/Program.cs
+++ lecture 1/lecture 1 another assembly/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using lecture_1;
 
 internal class Program
@@ -7,15 +9,57 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World!");
-        lecture_1.Methods myMethod = new Methods();
-        Methods.func1();
+        Car myCar = new Car();
+        Console.WriteLine($"my default  car is: {myCar.carModel}");
+        myCar.carModel = "my second model car";
+        Console.WriteLine($"my changed modal is: {myCar.carModel}");
+        Console.WriteLine($"my car price: {myCar.Price}");
+        myCar.Price= 30000; 
+        Console.WriteLine($"my changed price is: {myCar.Price}");
+        Console.WriteLine($"my car default year is: {myCar.Year}");
+
+        randomNumberTest();
+
     }
 
-    public class diffAssembly : Methods
+    private static void randomNumberTest()
     {
-        public diffAssembly()
+         Random myRandGen = new Random();
+        Dictionary<int, int> dicNumbers = new Dictionary<int, int>();
+        Stopwatch timer = new Stopwatch();
+        timer.Start();
+        for (int i = 0; i < 10000; i++)
         {
-           
+           var vrNumber =  myRandGen.Next(0, 101);
+            try
+            {
+                dicNumbers.Add(vrNumber, 1);
+            }
+            catch(Exception ex)
+            {
+                dicNumbers[vrNumber]++;
+            }
         }
+        timer.Stop();
+        Console.WriteLine($"elapsed time with try catch error {timer.Elapsed.TotalMilliseconds.ToString("NO")}");
+    }
+    class Car
+    {
+        public string carModel = "toyota";
+        public int Price { get; set; }
+        private int _Year = 1999;
+
+        public int Year
+        {
+            get {if (_Year < 0)
+                    _Year = 2000;
+                    return _Year;} //get method
+
+            set {
+                _Year = value - 10;
+                if ( _Year > 3000)
+                _Year = 3000;
+                  } //set method
+        } 
     }
 }
--- lecture 1/lecture 1/obj/Release/net6.0-windows/App.g.cs
+++ lecture 1/lecture 1/obj/Release/net6.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace lecture_1 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.3.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace lecture_1 {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.3.0")]
         public static void Main() {
             lecture_1.App app = new lecture_1.App();
             app.InitializeComponent();
